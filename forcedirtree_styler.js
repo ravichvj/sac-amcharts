118deae665a8a5c0d01374d975db732f627313d9
ravichvj <vsarathy.ravi@gmail.com>
2021-07-02 19:43:52 +0530

    Styler Fixed

--- forcedirtree_styler.js
+++ forcedirtree_styler.js
@@ -1,4 +1,3 @@
-/*
 (function()  {
 	let template = document.createElement("template");
 	template.innerHTML = `
@@ -47,30 +46,4 @@
 customElements.define("com-amcharts-forcedirtree-styling", ForcedirtreeStylingPanel);
 }) ();
 
-*/
-(function(){
-	let template = document.createElement("template");
-	template.innerHTML = `
-		<form id="form">
-			<fieldset>
-				<legend>Force directed Tree Properties</legend>
-				<table>
-					<tr>
-						<td>Color</td>
-						<td><input id="styling_color" type="text" size="40" maxlength="40"></td>
-					</tr>
-				</table>
-				<input type="submit" style="display:none;">
-			</fieldset>
-		</form>
-	`;
-	class ForcedirtreeStylingPanel extends HTMLElement {
-		constructor() {
-			super();
-			this._shadowRoot = this.attachShadow({mode: "open"});
-			this._shadowRoot.appendChild(template.content.cloneNode(true));			
-		}
-	}	
-	customElements.define("com-amcharts-forcedirtree-styling", ForcedirtreeStylingPanel);
 
-}) ();
