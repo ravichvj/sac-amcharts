816ed3878359ffaaf25afb7d78f6d9fcc0fa82d7
ravichvj <vsarathy.ravi@gmail.com>
2021-07-19 15:06:34 +0530

    ConfigComp_1

---
+++ ConfigComp/ConfigComp_builder.js
@@ -0,0 +1,63 @@
+(function()  {
+	let template = document.createElement("template");
+	template.innerHTML = `
+		<form id="form">
+			<table>
+				<tr>
+					<td>Chart Title</td>
+					<td><input id="chart_title" type="text"></td>
+				</tr>
+				<tr>
+					<td>Chart Title Font Size</td>
+					<td><input id="chart_title_fontsize" type="number" size="2" maxlength="2"></td>
+				</tr>
+			</table>
+			<input type="submit" style="display:none;">
+		</form>
+		<style>
+		:host {
+			display: block;
+			padding: 1em 1em 1em 1em;
+		}
+		</style>
+	`;
+
+	class ForcedirtreeBuilderPanel extends HTMLElement {
+		constructor() {
+			super();
+			this._shadowRoot = this.attachShadow({mode: "open"});
+			this._shadowRoot.appendChild(template.content.cloneNode(true));
+			this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
+		}
+
+		_submit(e) {
+			e.preventDefault();
+			this.dispatchEvent(new CustomEvent("propertiesChanged", {
+					detail: {
+						properties: {
+							title: this.title,
+							titlefontsize: this.titlefontsize
+						}
+					}
+			}));
+		}
+
+		set title(newTitle) {
+			this._shadowRoot.getElementById("chart_title").value = newTitle;
+		}
+
+		get title() {
+			return this._shadowRoot.getElementById("chart_title").value;
+		}
+		
+		set titlefontsize(newTitleFontSize) {
+			this._shadowRoot.getElementById("chart_title_fontsize").value = newTitleFontSize;
+		}
+
+		get titlefontsize() {
+			return this._shadowRoot.getElementById("chart_title_fontsize").value;
+		}
+	}
+
+	customElements.define("com-amcharts-forcedirtree-builder", ForcedirtreeBuilderPanel);
+})();
